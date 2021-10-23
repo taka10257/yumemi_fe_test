04091bd75eb3e4e35cb41ae30c6ee89ae35d79f8
takamasa suzuki <midgard10257@gmail.com>
2021-10-23 17:57:02 +0900

    外部APIに接続するモジュールのディレクトリ名を`api` -> `apis` へ変更した際にテストエラーになっていたのでjestの設定を修正

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/pages', '<rootDir>/api'],
+  roots: ['<rootDir>/pages', '<rootDir>/apis'],
   setupFilesAfterEnv: ['<rootDir>/test/setupTests.ts'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   transform: {
