c3e5c518af121d55016820d580645ddadd354b71
takamasa suzuki <midgard10257@gmail.com>
2021-10-23 13:24:12 +0900

    画面側から呼び出される都道府県データ取得APIの作成

---
+++ pages/api/prefectures.ts
@@ -0,0 +1,26 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { Prefecture } from '../../types/Prefecture'
+import { RESASApiPrefectures } from '../../api/resas/RESASApi'
+import { ApiError } from '../../types/pages/ApiError'
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Prefecture[] | ApiError>
+) {
+  await RESASApiPrefectures()
+    .then((value: Prefecture[] | undefined) => {
+      if (!value || value.length === 0) {
+        setErrorResponse(res)
+        return
+      }
+      res.status(200).json(value)
+    })
+    .catch((err) => {
+      console.log(err)
+      setErrorResponse(res)
+    })
+}
+
+function setErrorResponse(res: NextApiResponse<Prefecture[] | ApiError>): void {
+  res.status(500).json({ errorMessage: 'エラーが発生しました' })
+}
