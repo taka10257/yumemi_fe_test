4da311fdfa585c0dcdfaf959aa948559b1d4969a
takamasa suzuki <midgard10257@gmail.com>
2021-10-24 21:28:49 +0900

    アプリケーション共通のheadタグコンポーネントの追加

---
+++ components/organisms/header/MetaHead.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Head from 'next/head'
+
+export default function MetaHead() {
+  return (
+    <Head>
+      <title>都道府県別の総人口推移</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  )
+}
