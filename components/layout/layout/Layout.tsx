1b300a857114caf63380e9bcbc24b3222b031153
takamasa suzuki <midgard10257@gmail.com>
2021-10-24 22:02:42 +0900

    レイアウトに共通フッターを追加

--- components/layout/layout/Layout.tsx
+++ components/layout/layout/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react'
 import AppHeader from '../../organisms/header/AppHeader'
 import MetaHead from '../../organisms/header/MetaHead'
+import AppFooter from '../../organisms/footer/AppFooter'
 
 type LayoutProps = {
   children: ReactNode
@@ -12,6 +13,7 @@ export default function Layout({ children }: LayoutProps) {
       <MetaHead />
       <AppHeader />
       <>{children}</>
+      <AppFooter />
     </>
   )
 }
---
+++ components/organisms/footer/AppFooter.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './AppFooter.module.sass'
+
+export default function AppFooter() {
+  return (
+    <div className={styles.root}>
+      <p className={styles.title}>
+        U+E38286U+E38281U+E381BFU+E38195U+E38293U+E38182U+E3828AU+E3818CU+E381A8U+E38186
+      </p>
+    </div>
+  )
+}
